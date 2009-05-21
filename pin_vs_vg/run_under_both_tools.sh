833424192fbdf368583f11c6fc3b7e61f92dcd72
timurrrr <timurrrr@93a3164d-3a44-0410-8ae5-4baa038b0729>
2009-05-21 08:17:50 +0000

    Added TIMEOUT for tests.
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@999 93a3164d-3a44-0410-8ae5-4baa038b0729

--- pin_vs_vg/run_under_both_tools.sh
+++ pin_vs_vg/run_under_both_tools.sh
@@ -5,6 +5,7 @@
 
 COMMAND=$@
 NUM_DIVISIONS=${NUM_DIVISIONS:-"5"}
+TIMEOUT=2100
 
 if [ "$OUTPUT_PREFIX" == "" ]; then
   mkdir results 2>/dev/null
@@ -13,7 +14,7 @@ if [ "$OUTPUT_PREFIX" == "" ]; then
 fi
 
 export TIMEFORMAT="VALGRIND TIME: %R"
-time ./valgrind/inst/bin/valgrind --tool=testgrind --N=${NUM_DIVISIONS} --log-file="${OUTPUT_PREFIX}_valgrind.log" $@ 2>&1 | grep "TIME:"
+time alarm $TIMEOUT ./valgrind/inst/bin/valgrind --tool=testgrind --N=${NUM_DIVISIONS} --log-file="${OUTPUT_PREFIX}_valgrind.log" $@ 2>&1 | grep "TIME:"
 
 export TIMEFORMAT="PIN TIME: %R"
-time ./pin/pin -t pin_tool/pin_instrument_test.so -logfile "${OUTPUT_PREFIX}_pin.log" -N ${NUM_DIVISIONS} -- $@ 2>&1 | grep "TIME: "
+time alarm $TIMEOUT ./pin/pin -t pin_tool/pin_instrument_test.so -logfile "${OUTPUT_PREFIX}_pin.log" -N ${NUM_DIVISIONS} -- $@ 2>&1 | grep "TIME: "
