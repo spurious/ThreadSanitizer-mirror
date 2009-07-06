f334d296df0e4d5ad827562c7460af075c40d519
konstantin.s.serebryany <konstantin.s.serebryany@93a3164d-3a44-0410-8ae5-4baa038b0729>
2009-07-06 08:41:24 +0000

    simple script to test all 3 modes of ThreadSanitizer with racecheck_unittest
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@1075 93a3164d-3a44-0410-8ae5-4baa038b0729

---
+++ unittest/test_all.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+make
+
+export MSM_THREAD_SANITIZER=1
+PARAM="$@"
+
+run() {
+  env $ENV tsan $FLAGS \
+    --error-exitcode=1 --gen-suppressions=all \
+    --suppressions=racecheck_unittest.supp \
+    ./racecheck_unittest $PARAM || exit 1
+}
+
+fast_mode() {
+  ENV=TSAN_FAST_MODE=1
+  FLAGS="--fast-mode=yes --pure-happens-before=no --ignore-in-dtor=yes"
+  run
+}
+
+slow_mode() {
+  ENV=
+  FLAGS="--fast-mode=no  --pure-happens-before=no --ignore-in-dtor=no"
+  run
+}
+
+pure_hb_mode() {
+  ENV=TSAN_PURE_HAPPENS_BEFORE=1
+  FLAGS="--pure-happens-before=yes --ignore-in-dtor=no"
+  run
+}
+
+fast_mode
+slow_mode
+pure_hb_mode
+
