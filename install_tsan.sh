2cccf5b1d8050f5276936d846d2a2adb41894a16
ramosian.glider <ramosian.glider@93a3164d-3a44-0410-8ae5-4baa038b0729>
2009-07-22 15:05:05 +0000

    Patching valgrind/VEX with tsan/vex.patch
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@1093 93a3164d-3a44-0410-8ae5-4baa038b0729

--- install_tsan.sh
+++ install_tsan.sh
@@ -2,9 +2,21 @@
 # Script to add ThreadSanitizer into existing valgrind directory
 
 VALGRIND_REV=10454
+VEX_REV=1908
 TSAN_REV=1089
 VALGRIND_DIR=`pwd`
 
+# Check that the SVN version in $VALGRIND_DIR/VEX is equal to $VEX_REV
+cd VEX && if ! svn info | grep "Revision: ${VEX_REV}" >/dev/null
+then
+  echo "WARNING:" >&2
+  echo "Either you run the script from outside of the valgrind directory" >&2
+  echo "or the VEX revision number is different the one we used (r${VEX_REV})." >&2
+  echo "The patch may not apply" >&2
+fi
+cd $VALGRIND_DIR
+
+# Check that the SVN version in $VALGRIND_DIR is equal to $VALGRIND_REV
 if ! svn info | grep "Revision: ${VALGRIND_REV}" >/dev/null
 then
   echo "WARNING:" >&2
@@ -33,3 +45,6 @@ touch docs/Makefile.am tests/Makefile.am
 # Apply a patch to the valgrind files.
 cd ../
 patch -p 0 < tsan/valgrind.patch
+# Apply a patch to the VEX files.
+cd VEX
+patch -p 0 < ../tsan/vex.patch
