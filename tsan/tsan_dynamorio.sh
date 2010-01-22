5b5607cd67f51961bb0ef10a03c4e6621fce6d93
konstantin.s.serebryany@gmail.com <konstantin.s.serebryany@gmail.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-01-22 15:15:16 +0000

    autodetect bitness in tsa_dynamorio.sh
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@1486 93a3164d-3a44-0410-8ae5-4baa038b0729

--- tsan/tsan_dynamorio.sh
+++ tsan/tsan_dynamorio.sh
@@ -4,8 +4,39 @@ DYNAMORIO_ROOT=${DYNAMORIO_ROOT:=$HOME/DynamoRIO}
 TS_ROOT=${TS_ROOT:-.}
 TS_VARIANT=-debug
 
-TS_FLAGS=
+TS_FLAGS=" "
 
-$DYNAMORIO_ROOT/bin32/drdeploy  -client \
-   $TS_ROOT/bin/x86-linux-debug-ts_dynamorio.so 0 "$TS_FLAGS" \
-   "$@"
+for arg in "$@"; do
+  case $arg in
+    --opt) TS_VARIANT="";;
+    --dbg) TS_VARIANT="-debug";;
+    --) shift; break;;
+    -64) ARCH="amd64"; BITNESS="64";;
+    -32) ARCH="x86"; BITNESS="32";;
+    -*) TS_FLAGS="$TS_FLAGS $arg";;
+    *) break;;
+  esac
+  shift
+done
+
+PROGRAM="$1"
+shift
+PARAMS="$@"
+
+# detect bitness if not given explicitly.
+if [ "$BITNESS" == "" ]; then
+  file_format=`objdump -f  $PROGRAM | grep -o 'file format elf.*'`
+  echo $file_format
+  if [ "$file_format" == "file format elf64-x86-64" ]; then
+    BITNESS=64
+    ARCH=amd64
+  else 
+    BITNESS=32
+    ARCH=x86
+  fi
+fi
+
+
+$DYNAMORIO_ROOT/bin$BITNESS/drdeploy \
+   -client $TS_ROOT/bin/$ARCH-linux-debug-ts_dynamorio.so 0 "$TS_FLAGS" \
+   $PROGRAM $PARAMS
