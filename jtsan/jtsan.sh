e63e79fcd3082b578db9af66a57545b17b8fb2a7
konstantin.s.serebryany@gmail.com <konstantin.s.serebryany@gmail.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-01-25 10:31:12 +0000

    added a simple ThreadSanitizerJava driver
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@1491 93a3164d-3a44-0410-8ae5-4baa038b0729

---
+++ jtsan/jtsan.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+JTSAN_ROOT=${JTSAN_ROOT:-../third_party/java-thread-sanitizer/dist}
+JTSAN_LOGFILE=${JTSAN_LOGFILE:-jtsan.events}
+java \
+  -Xbootclasspath/p:$JTSAN_ROOT/agent.jar \
+  -javaagent:$JTSAN_ROOT/agent.jar=-logfile=$JTSAN_LOGFILE \
+  "$@"
