2b05b4eda21ad9197c3d12030ff280e9be139289
eugenis@google.com <eugenis@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-02-16 13:08:46 +0000

    Add a fast linux builder. Fix output test.
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@1675 93a3164d-3a44-0410-8ae5-4baa038b0729

--- buildbot/builder_linux_small.py
+++ buildbot/builder_linux_small.py
@@ -26,26 +26,6 @@ def generate(settings):
                           description='installing tsan',
                           descriptionDone='install tsan'))
 
-  # Test that mk-self-contained-tsan works. Output is unused.
-  f1.addStep(ShellCommand(command=['tsan_binary/mk-self-contained-tsan.sh',
-                                   'out', 'tsan.sh'],
-                          description='packing self-contained tsan',
-                          descriptionDone='pack self-contained tsan'))
-
-  # Build 32-bit valgrind+tsan and install them to out32/.
-  f1.addStep(ShellCommand(command='cd third_party && ' +
-                          './build_and_install_valgrind.sh `pwd`/../out32 --enable-only32bit',
-                          description='building 32-bit valgrind',
-                          descriptionDone='build 32-bit valgrind'))
-  f1.addStep(Compile(command=['make', '-C', 'tsan', '-j4', 'OFFLINE=',
-                              'PIN_ROOT=', 'lo', 'ld'],
-                     description='building 32-bit tsan',
-                     descriptionDone='build 32-bit tsan'))
-  f1.addStep(ShellCommand(command=['make', '-C', 'tsan', 'install',
-                                   'VALGRIND_INST_ROOT=../out32'],
-                          description='installing 32-bit tsan',
-                          descriptionDone='install 32-bit tsan'))
-
   # Run output tests.
   output_test_binary = unitTestBinary('linux', 64, 0, False, test_base_name='output_test1')
   output_test_desc = addBuildTestStep(f1, 'linux', 64, 0, False);
@@ -61,12 +41,7 @@ def generate(settings):
   os = 'linux'
   #                  test binary | tsan + run parameters
   #             bits, opt, static,   tsan-debug,   mode
-  variants = [((  64,   1, False),(        True, 'fast')),
-              ((  64,   1, False),(        True, 'slow')),
-              ((  64,   1, False),(        True,  'phb')),
-              ((  32,   1, False),(        True, 'slow')),
-              ((  64,   0, False),(        True, 'slow')),
-              ((  64,   1, False),(       False,  'phb'))]
+  variants = [((  64,   0, False),(        False, 'fast'))]
   for (test_variant, run_variant) in variants:
     (tsan_debug, mode) = run_variant
     if not test_binaries.has_key(test_variant):
@@ -77,15 +52,9 @@ def generate(settings):
     addTestStep(f1, tsan_debug, mode, test_binary, test_desc, extra_args=["--error_exitcode=1"])
 
 
-  # Run unit tests with 32-bit valgrind.
-  test_desc = test_binaries[(32, 1, False)]
-  test_binary = unitTestBinary(os, 32, 1, False)
-  addTestStep(f1, False, 'fast', test_binary, test_desc + '(32-bit valgrind)',
-              frontend_binary='out32/bin/valgrind')
-
-  b1 = {'name': 'buildbot-linux',
-        'slavename': 'bot1name',
-        'builddir': 'full',
+  b1 = {'name': 'buildbot-linux-small',
+        'slavename': 'bot5name',
+        'builddir': 'full_linux_small',
         'factory': f1,
         }
 
