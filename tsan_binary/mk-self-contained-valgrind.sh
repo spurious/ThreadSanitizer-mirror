daa5aaeb8802e799f5cc4e01d04b1feda0e193e4
eugenis@google.com <eugenis@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-02-27 12:16:24 +0000

    Simplify mk-self-contained... script, and fix error reporting.
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@1772 93a3164d-3a44-0410-8ae5-4baa038b0729

--- tsan_binary/mk-self-contained-valgrind.sh
+++ tsan_binary/mk-self-contained-valgrind.sh
@@ -67,6 +67,6 @@ __COMPRESSED_DATA_BELOW__
 EOF
 
 # Dump the compressed binary at the very end of the file.
-(cd $IN_DIR && tar zcvh $IN_FILES || exit 1) | cat >> $OUT
+(cd $IN_DIR && tar zcvh $IN_FILES) >> $OUT || exit 1
 
 echo "File $OUT successfully created"
