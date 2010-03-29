7de0db3996f59c75093a9cb9f96870a77ef84ae7
eugenis@google.com <eugenis@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-03-29 12:28:49 +0000

    Add a script fo running non-debug tsan_pin on Windows. Build both debug and non-debug tsan on Windows by default.
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@1917 93a3164d-3a44-0410-8ae5-4baa038b0729

---
+++ tsan/tsan_pin-debug.bat
@@ -0,0 +1,2 @@
+
+%PIN_ROOT%/pin -t bin/x86-windows-debug-ts_pin.dll -short_name -- %1 %2 %3 %4 %5
--- tsan/tsan_pin.bat
+++ tsan/tsan_pin.bat
@@ -1,2 +1,2 @@
 
-%PIN_ROOT%/pin -t bin/x86-windows-debug-ts_pin.dll -short_name -- %1 %2 %3 %4 %5
+%PIN_ROOT%/pin -t bin/x86-windows-ts_pin.dll -short_name -- %1 %2 %3 %4 %5
