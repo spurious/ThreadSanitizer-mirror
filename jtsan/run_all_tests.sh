e6fbb5f05ba5e9c5567a24df5843b12bed719d76
egor.pasko <egor.pasko@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-04-11 20:10:02 +0000

    Add a trivial ThreadSanitizerTest testrunner.
    * 32bit TSAN only for now
    * unable to run individual tests
    * deletes the test output directory after reporting results
    
    
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@1976 93a3164d-3a44-0410-8ae5-4baa038b0729

---
+++ jtsan/run_all_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Run all tests in ThreadSanitizerTest and report results.
+
+JTSAN_ROOT="../third_party/java-thread-sanitizer/dist"
+TSAN_OFFLINE="../tsan/bin/x86-linux-debug-ts_offline"
+TMPDIR="/tmp/$(basename $0).$$"
+JAVALOG="$TMPDIR/java.log"
+JTSAN_EVENTS="$TMPDIR/jtsan.events"
+TSAN_OUT="$TMPDIR/tsan.out"
+
+set -x
+mkdir $TMPDIR
+java \
+  -Xbootclasspath/p:$JTSAN_ROOT/agent.jar \
+  -javaagent:$JTSAN_ROOT/agent.jar=logfile=${JTSAN_EVENTS} \
+  ThreadSanitizerTest >"$JAVALOG" 2>&1
+${TSAN_OFFLINE} < ${JTSAN_EVENTS} > ${TSAN_OUT}
+
+./summarize.py "$TMPDIR"
+
+rm -rf "$TMPDIR"
+set +x
