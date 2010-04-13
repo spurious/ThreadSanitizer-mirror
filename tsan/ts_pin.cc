10c0e0dc9da53e193f8a55147d540efcb47d103c
timurrrr <timurrrr@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-04-13 14:51:13 +0000

    Once again: fix false reports on Thread/Join on PIN
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@1982 93a3164d-3a44-0410-8ae5-4baa038b0729

--- tsan/ts_pin.cc
+++ tsan/ts_pin.cc
@@ -1004,11 +1004,22 @@ void CallbackForThreadFini(THREADID tid, const CONTEXT *ctxt,
 }
 
 static bool HandleThreadJoinAfter(THREADID tid, pthread_t joined_ptid) {
-  THREADID joined_tid = 0;
-  for (joined_tid = 1; joined_tid < kMaxThreads; joined_tid++) {
-    if (g_pin_threads[joined_tid].thread_finished == false) continue;
-    if (g_pin_threads[joined_tid].my_ptid == joined_ptid)
-      break;
+  THREADID joined_tid = kMaxThreads;
+  int max_uniq_tid_found = -1;
+
+  // TODO(timurrrr): walking through g_pin_threads may be slow.
+  // Do we need to/Can we optimize it?
+  for (THREADID j = 1; j < kMaxThreads; j++) {
+    if (g_pin_threads[j].thread_finished == false)
+      continue;
+    if (g_pin_threads[j].my_ptid == joined_ptid) {
+      // We search for the thread with the maximum uniq_tid to work around
+      // thread HANDLE reuse issues.
+      if (max_uniq_tid_found < g_pin_threads[j].uniq_tid) {
+        max_uniq_tid_found = g_pin_threads[j].uniq_tid;
+        joined_tid = j;
+      }
+    }
   }
   if (joined_tid == kMaxThreads) {
     // This may happen in the following case:
