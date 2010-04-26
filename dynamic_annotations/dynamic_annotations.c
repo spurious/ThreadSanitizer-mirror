69dd979ba6a9eb49cdd07ab23283e831954fd391
timurrrr <timurrrr@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-04-26 09:44:48 +0000

    Fix compile-time errors on Linux
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@2020 93a3164d-3a44-0410-8ae5-4baa038b0729

--- dynamic_annotations/dynamic_annotations.c
+++ dynamic_annotations/dynamic_annotations.c
@@ -132,13 +132,15 @@ static int GetRunningOnValgrind(void) {
     return strcmp(running_on_valgrind_str, "0") != 0;
   }
 #else
-  // Visual Studio issues warnings if we use getenv,
-  // so we use GetEnvironmentVariableA instead.
+  /* Visual Studio issues warnings if we use getenv,
+   * so we use GetEnvironmentVariableA instead.
+   */
   char value[100] = "1";
   int res = GetEnvironmentVariableA("RUNNING_ON_VALGRIND",
                                     value, sizeof(value));
-  // value will remain "1" if res == 0 or res >= sizeof(value). The latter
-  // can happen only if the given value is long, in this case it can't be "0".
+  /* value will remain "1" if res == 0 or res >= sizeof(value). The latter
+   * can happen only if the given value is long, in this case it can't be "0".
+   */
   if (res > 0 && !strcmp(value, "0"))
     return 1;
 #endif
