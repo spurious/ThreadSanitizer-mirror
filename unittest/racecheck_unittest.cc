8794701860119e50ab6b150e652c65b1aed86fdf
timurrrr <timurrrr@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-04-28 12:33:15 +0000

    Quick follow-up: call both 32-bit and 64-bit versions of lstrlen
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@2031 93a3164d-3a44-0410-8ae5-4baa038b0729

--- unittest/racecheck_unittest.cc
+++ unittest/racecheck_unittest.cc
@@ -3111,7 +3111,8 @@ void WorkerX() {
   CHECK(rindex(str, 'x') == str+3);
   CHECK(rindex(str, 'Y') == NULL);
 #else
-  CHECK(lstrlen(NULL) == 0); 
+  CHECK(lstrlenA(NULL) == 0); 
+  CHECK(lstrlenW(NULL) == 0); 
 #endif
   CHECK(strchr(str, 'X') == str);
   CHECK(strchr(str, 'x') == str+1);
