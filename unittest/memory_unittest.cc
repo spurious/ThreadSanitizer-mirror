ca8315a6c0c8e42663a74d86ece16b0260fffab4
timurrrr <timurrrr@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-05-14 13:47:20 +0000

    Add a couple of simple memory error detection -related tests. These tests fail under DrMemory
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@2068 93a3164d-3a44-0410-8ae5-4baa038b0729

---
+++ unittest/memory_unittest.cc
@@ -0,0 +1,66 @@
+/* Copyright (c) 2010, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ---
+ * Author: Timur Iskhodzhanov
+ *
+ * This file contains a set of unit tests for memory error detection tools.
+ */
+
+#include "test_utils.h"
+
+#include <gtest/gtest.h>
+
+void Noop() {}
+
+TEST(Wrappers, StrchrTest) {
+  // DrMemory bug http://code.google.com/p/dynamorio/issues/detail?id=275
+  char foo[8] = {10, 20, 127, 128, 250, -50, 0};
+  CHECK(strchr(foo, 10) != 0);
+  CHECK(strchr(foo, 127) != 0);
+  CHECK(strchr(foo, 128) != 0);
+  CHECK(strchr(foo, 250) != 0);
+  CHECK(strchr(foo, -50) != 0);
+  CHECK(strchr(foo, -60) == 0);
+  CHECK(strchr(foo, 0) != 0);
+  CHECK(strrchr(foo, 10) != 0);
+  CHECK(strrchr(foo, 0) != 0);
+  CHECK(strrchr(foo, 250) != 0);
+  CHECK(strrchr(foo, -60) == 0);
+}
+
+TEST(Threads, EmptyThreadTest) {
+  // DrMemory bug http://code.google.com/p/dynamorio/issues/detail?id=286
+  MyThread mt(Noop);
+  mt.Start();
+  mt.Join();
+}
+
+#ifdef WIN32
+TEST(SyscallTests, OutputDebugStringTest) {
+  // DrMemory bug http://code.google.com/p/dynamorio/issues/detail?id=281
+  OutputDebugString("Hello!\n");
+}
+#endif
