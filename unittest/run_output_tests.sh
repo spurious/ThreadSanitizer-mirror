e12b86567a734b54bc2cb55374caf88894b69e2d
timurrrr <timurrrr@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-05-20 13:56:09 +0000

    Fix run_output_tests.sh for Windows
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@2109 93a3164d-3a44-0410-8ae5-4baa038b0729

--- unittest/run_output_tests.sh
+++ unittest/run_output_tests.sh
@@ -13,7 +13,7 @@ do
     IGNORE=--ignore=output_tests/${TEST}.ignore
   fi
 
-  CMD="$TSAN $SUPP $IGNORE ./${BIN}output_tests/${TEST}-${BUILD}${EXE} 2>&1 | ./match_output.py output_tests/${TEST}.tmpl"
+  CMD="$TSAN $SUPP $IGNORE -- ./${BIN}output_tests/${TEST}-${BUILD}${EXE} 2>&1 | python match_output.py output_tests/${TEST}.tmpl"
   echo "Running"
   echo "$ $CMD"
   $CMD
