ad363714d26d92619812e22d9cc36714f872a239
sergeyvorobyev@google.com <sergeyvorobyev@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-08-04 10:01:08 +0000

    Remove semaphore test from Target tests
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@2363 93a3164d-3a44-0410-8ae5-4baa038b0729

--- jtsan/jtsan.sh
+++ jtsan/jtsan.sh
@@ -3,5 +3,5 @@ JTSAN_ROOT=${JTSAN_ROOT:-`dirname $0`/../third_party/java-thread-sanitizer/dist}
 JTSAN_LOGFILE=${JTSAN_LOGFILE:-jtsan.events}
 java \
   -Xbootclasspath/p:$JTSAN_ROOT/agent.jar \
-  -javaagent:$JTSAN_ROOT/agent.jar=-logfile=$JTSAN_LOGFILE \
+  -javaagent:$JTSAN_ROOT/agent.jar=logfile=$JTSAN_LOGFILE:writer=str \
   "$@"
