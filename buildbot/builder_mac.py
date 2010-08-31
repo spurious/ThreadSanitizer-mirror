eaceddd2fe178e52585d88d9e95a47d0f12e044a
eugenis@google.com <eugenis@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-08-31 10:37:25 +0000

    Enable 32-bit RaceVerifier tests on linux and mac bots.
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@2413 93a3164d-3a44-0410-8ae5-4baa038b0729

--- buildbot/builder_mac.py
+++ buildbot/builder_mac.py
@@ -79,14 +79,14 @@ def generate(settings):
     test_binary = unitTestBinary(os, bits, opt, static)
     addTestStep(f1, tsan_debug, mode, test_binary, test_desc,
                 extra_args=["--error_exitcode=1"])
-    # addTestStep(f1, tsan_debug, mode, test_binary, test_desc + ' RV 1st pass',
-    #             extra_args=['--show-expected-races', '--error_exitcode=1'],
-    #             extra_test_args=['--gtest_filter="RaceVerifierTests.*"'],
-    #             append_command='2>&1 | tee raceverifier.log')
-    # addTestStep(f1, tsan_debug, mode, test_binary, test_desc + ' RV 2nd pass',
-    #             extra_args=['--error_exitcode=1', '--race-verifier=raceverifier.log'],
-    #             extra_test_args=['--gtest_filter="RaceVerifierTests.*"'],
-    #             append_command='2>&1')
+    addTestStep(f1, tsan_debug, mode, test_binary, test_desc + ' RV 1st pass',
+                extra_args=['--show-expected-races', '--error_exitcode=1'],
+                extra_test_args=['--gtest_filter="RaceVerifierTests.*"'],
+                append_command='2>&1 | tee raceverifier.log')
+    addTestStep(f1, tsan_debug, mode, test_binary, test_desc + ' RV 2nd pass',
+                extra_args=['--error_exitcode=1', '--race-verifier=raceverifier.log'],
+                extra_test_args=['--gtest_filter="RaceVerifierTests.*"'],
+                append_command='2>&1')
 
 
   b1 = {'name': 'buildbot-mac',
