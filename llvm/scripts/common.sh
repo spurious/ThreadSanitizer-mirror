2d56338375f42147e7ab95a73c55feb96b3fdd97
ramosian.glider <ramosian.glider@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-11-26 14:08:59 +0000

    Move the compile-time definitions for dynamic annotations to the wrappers for gcc and g++.
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@2771 93a3164d-3a44-0410-8ae5-4baa038b0729

--- llvm/scripts/common.sh
+++ llvm/scripts/common.sh
@@ -21,6 +21,9 @@ LLC=llc
 LINK_DBG="$SCRIPT_ROOT/link_debuginfo.sh"
 TSAN_RTL="$SCRIPT_ROOT/../tsan_rtl/tsan_rtl32.a"
 
+# Additional flags for dynamic annotations.
+DA_FLAGS="-DDYNAMIC_ANNOTATIONS_WANT_ATTRIBUTE_WEAK -DRACECHECK_UNITTEST_WANT_ATTRIBUTE_WEAK -DDYNAMIC_ANNOTATIONS_PREFIX=LLVM"
+
 PLATFORM="x86"
 set_platform_dependent_vars () {
   if [ "$PLATFORM" == "x86-64" ]
