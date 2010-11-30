44deee9fb69cee50fdbd47950f490c396f550b83
ramosian.glider <ramosian.glider@93a3164d-3a44-0410-8ae5-4baa038b0729>
2010-11-30 10:19:37 +0000

    Remove dead code related to per-block instrumentation.
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@2807 93a3164d-3a44-0410-8ae5-4baa038b0729

--- llvm/opt/ThreadSanitizer/ThreadSanitizer.cpp
+++ llvm/opt/ThreadSanitizer/ThreadSanitizer.cpp
@@ -54,9 +54,8 @@ namespace {
 
   struct TsanOnlineInstrument : public ModulePass { // {{{1
     static char ID; // Pass identification, replacement for typeid
-    int BBCount, ModuleFunctionCount, ModuleMopCount, TLEBIndex, OldTLEBIndex;
-    Value *BBPassportGlob, *TracePassportGlob;
-    int BBNumMops;
+    int ModuleFunctionCount, ModuleMopCount, TLEBIndex, OldTLEBIndex;
+    Value *TracePassportGlob;
     int TraceCount, TraceNumMops;
     Constant *BBFlushFn;
     Constant *RtnCallFn, *RtnExitFn;
@@ -436,7 +435,6 @@ namespace {
     }
 
     virtual bool runOnModule(Module &M) {
-      BBCount = 0;
       TraceCount = 0;
       ModuleFunctionCount = 0;
       ModuleMopCount = 0;
@@ -535,7 +533,6 @@ namespace {
 
       for (Module::iterator F = M.begin(), E = M.end(); F != E; ++F) {
         ModuleFunctionCount++;
-        int startBBCount = BBCount;
         int startTraceCount = TraceCount;
         bool first_dtor_bb = false;
 
@@ -550,7 +547,6 @@ namespace {
         }
         if (isDtor(F->getName())) first_dtor_bb = true;
 
-#if 1
         TraceVector traces(buildTraces(*F));
         for (int i = 0; i < traces.size(); ++i) {
           runOnTrace(M, traces[i], first_dtor_bb);
@@ -560,17 +556,6 @@ namespace {
         std::vector<Value*> inst(1);
         inst[0] = ConstantInt::get(PlatformInt, getAddr(startTraceCount, 0, First));
         CallInst::Create(RtnCallFn, inst.begin(), inst.end(), "", First);
-#else
-        for (Function::iterator BB = F->begin(), E = F->end(); BB != E; ++BB) {
-          runOnBasicBlockUnused(M, BB, first_dtor_bb);
-          first_dtor_bb = false;
-        }
-        Instruction* First = F->begin()->begin();
-        std::vector<Value*> inst(1);
-        // BBNumMops = 0 at the start of basic block.
-        inst[0] = ConstantInt::get(PlatformInt, getAddr(startBBCount, 0, First));
-        CallInst::Create(RtnCallFn, inst.begin(), inst.end(), "", First);
-#endif
       }
       writeDebugInfo(M);
 
@@ -639,27 +624,6 @@ namespace {
     bool isaCallOrInvoke(BasicBlock::iterator &BI) {
       return ((isa<CallInst>(BI) && (!isa<DbgDeclareInst>(BI))) ||
               isa<InvokeInst>(BI));
-    }
-
-
-    bool ShouldFlushSlice(BasicBlock::iterator &Begin,
-                          BasicBlock::iterator &End) {
-      for (BasicBlock::iterator BI = Begin, BE = End;
-           BI != BE; ++BI) {
-#if DEBUG
-        BI->dump();
-#endif
-        if (isa<LoadInst>(BI)) {
-          return true;
-        }
-        if (isa<StoreInst>(BI)) {
-          return true;
-        }
-        if (isaCallOrInvoke(BI)) {
-          return true;
-        }
-      }
-      return false;
     }
 
     bool shouldFlushTrace(Trace &trace) {
@@ -778,99 +742,6 @@ namespace {
       return false;
     }
 
-    bool MakePassportFromSlice(Module &M,
-                               BasicBlock::iterator &Begin,
-                               BasicBlock::iterator &End) {
-      Passport passport;
-      bool isStore, isMop;
-      int size, src_size, dest_size;
-      BBNumMops = 0;
-      for (BasicBlock::iterator BI = Begin, BE = End;
-           BI != BE; ++BI) {
-        isMop = false;
-        if (isa<LoadInst>(BI)) {
-          isStore = false;
-          isMop = true;
-        }
-        if (isa<StoreInst>(BI)) {
-          isStore = true;
-          isMop = true;
-        }
-        if (isaCallOrInvoke(BI)) {
-          // CallInst or maybe other instructions that may cause a jump.
-          assert(false);
-        }
-        if (isMop) {
-          size = getArchSize();
-          BBNumMops++;
-          ModuleMopCount++;
-          Value *MopPtr;
-          llvm::Instruction &IN = *BI;
-          getAddr(BBCount, BBNumMops, BI);
-          if (isStore) {
-            MopPtr = (static_cast<StoreInst&>(IN).getPointerOperand());
-          } else {
-            MopPtr = (static_cast<LoadInst&>(IN).getPointerOperand());
-          }
-          if (MopPtr->getType()->isSized()) {
-            if
-              (cast<PointerType>(MopPtr->getType())->getElementType()->isSized())
-                {
-                  size =
-                    getAnalysis<TargetData>().getTypeStoreSizeInBits(
-                      cast<PointerType>(MopPtr->getType())->getElementType()
-                    );
-                }
-          }
-
-
-          if (MopPtr->getType() != UIntPtr) {
-            MopPtr = BitCastInst::CreatePointerCast(MopPtr, UIntPtr, "", BI);
-          }
-
-          std::vector<Constant*> mop;
-          mop.push_back(ConstantInt::get(PlatformInt, getAddr(BBCount, BBNumMops, BI)));
-          mop.push_back(ConstantInt::get(Int8, size / 8));
-          mop.push_back(ConstantInt::get(Int8, isStore));
-          passport.push_back(ConstantStruct::get(MopTy, mop));
-        }
-      }
-      if (BBNumMops) {
-        BBPassportType = ArrayType::get(MopTy, BBNumMops);
-        LLVMContext &Context = M.getContext();
-        BBTraceInfoType = StructType::get(Context,
-                                    PlatformInt, PlatformInt, PlatformInt, PlatformInt, Int8,
-                                    BBPassportType,
-                                    NULL);
-
-        std::vector<Constant*> trace_info;
-        // num_mops_
-        trace_info.push_back(ConstantInt::get(PlatformInt, BBNumMops));
-        // pc_
-        trace_info.push_back(ConstantInt::get(PlatformInt, getAddr(BBCount,
-                                                             0, Begin)));
-        // id_ == 0 initially.
-        trace_info.push_back(ConstantInt::get(PlatformInt, 0));
-        // counter_
-        trace_info.push_back(ConstantInt::get(PlatformInt, 0));
-        // generate_segments_
-        trace_info.push_back(ConstantInt::get(Int8, 1));
-        // mops_
-        trace_info.push_back(ConstantArray::get(BBPassportType, passport));
-
-        BBPassportGlob = new GlobalVariable(
-            M,
-            BBTraceInfoType,
-            false,
-            GlobalValue::InternalLinkage,
-            ConstantStruct::get(BBTraceInfoType, trace_info),
-            "bb_passport",
-            false, 0
-            );
-        return true;
-      }
-      return false;
-    }
 
     void InstrumentMop(BasicBlock::iterator &BI, bool isStore,
                        Value *TLEB, bool check_ident_store) {
@@ -958,78 +829,6 @@ namespace {
       return true;
     }
 
-    // Process a real basic block, i.e. a piece of code containing no calls.
-    // Returns true iff a call to bb_flush was inserted.
-    bool runOnBasicBlockSlice(Module &M,
-                              BasicBlock::iterator &Begin,
-                              BasicBlock::iterator &End,
-                              bool first_dtor_bb) {
-      bool result = false;
-      BBCount++;
-      OldTLEBIndex = 0;
-      TLEBIndex = 0;
-      Value *TLEB = NULL;
-      bool have_passport = MakePassportFromSlice(M, Begin, End);
-      if (ShouldFlushSlice(Begin, End)) {
-        BasicBlock::iterator First = Begin;
-#if DEBUG
-        errs() << "BI: " << First->getOpcodeName() << "\n";
-#endif
-        std::vector <Value*> Args(1);
-        std::vector <Value*> idx;
-        idx.push_back(ConstantInt::get(PlatformInt, 0));
-        if (have_passport) {
-          Args[0] =
-            GetElementPtrInst::Create(BBPassportGlob,
-                                      idx.begin(),
-                                      idx.end(),
-                                      "",
-                                      First);
-          Args[0] = BitCastInst::CreatePointerCast(Args[0], TraceInfoTypePtr,
-                                                   "", First);
-        } else {
-          Args[0] = ConstantPointerNull::get(TraceInfoTypePtr);
-        }
-
-        TLEB = CallInst::Create(BBFlushFn, Args.begin(), Args.end(), "",
-                           First);
-        result = true;
-      }
-
-#if DEBUG
-      errs() << "========BB("<< BBCount<<")===========\n";
-#endif
-
-      for (BasicBlock::iterator BI = Begin, BE = End;
-           BI != BE; ++BI) {
-        bool unknown = true;  // we just don't want a bunch of nested if()s
-        if (isa<LoadInst>(BI)) {
-          // Instrument LOAD.
-          InstrumentMop(BI, false, TLEB, first_dtor_bb);
-          unknown = false;
-        }
-        if (isa<StoreInst>(BI)) {
-          // Instrument STORE.
-          InstrumentMop(BI, true, TLEB, first_dtor_bb);
-          unknown = false;
-        }
-        // TODO(glider): invoke!
-        if (isaCallOrInvoke(BI) && unknown) {
-          // BBSlice can't contain a call or invoke instruction.
-          errs() << "Invalid instruction: ";
-          BI->dump();
-          assert(false);
-        }
-        if (unknown) {
-          // do nothing
-        }
-      }
-#if DEBUG
-      errs() << "========BB===========\n";
-#endif
-      return result;
-    }
-
     bool runOnBasicBlock(Module &M, BasicBlock *BB,
                          bool first_dtor_bb,
                          Value *TLEB) {
@@ -1078,75 +877,6 @@ namespace {
         if (unknown) {
           // do nothing
         }
-      }
-    }
-
-    bool runOnBasicBlockUnused(Module &M, Function::iterator &BB,
-                         bool first_dtor_bb) {
-      BasicBlock::iterator Begin = BB->begin(), End = BB->begin();
-      bool validBegin = false, validEnd = false;
-      bool dirty = false, new_dirty = false;
-      for (BasicBlock::iterator BI = BB->begin(), BE = BB->end();
-           BI != BE;
-           ++BI) {
-        if (!validBegin) {
-          Begin = BI;
-          validBegin = true;
-        }
-        bool unknown = true;  // we just don't want a bunch of nested if()s
-        if (isaCallOrInvoke(BI)) {
-          if (isa<MemTransferInst>(BI)) {
-            InstrumentMemTransfer(BI);
-          }
-          std::vector<Value*> inst(1);
-          llvm::Instruction &IN = *BI;
-          if ((isa<CallInst>(BI) &&
-               static_cast<CallInst&>(IN).getCalledFunction() == BBFlushFn) ||
-              (isa<InvokeInst>(BI) &&
-               static_cast<InvokeInst&>(IN).getCalledFunction() == BBFlushFn)) {
-            // TODO(glider): we shouldn't encounter BBFlushFn at all.
-            errs() << "BBFlushFn!\n";
-            assert(false);
-          }
-          // TODO(glider): should we invalidate first_dtor_bb after the first
-          // slice?
-          if (validBegin && validEnd) {
-            End = BI;
-            new_dirty = runOnBasicBlockSlice(M, Begin, End, first_dtor_bb);
-            dirty = dirty || new_dirty;
-          } else {
-            if (dirty) {
-              flushBeforeCall(M, BI);
-            }
-          }
-          validBegin = false;
-          validEnd = false;
-          unknown = false;
-        }
-
-        if (isa<ReturnInst>(BI)) {
-          if (validBegin && validEnd) {
-            End = BI;
-            new_dirty = runOnBasicBlockSlice(M, Begin, End, first_dtor_bb);
-            dirty = dirty || new_dirty;
-          }
-          validBegin = false;
-          validEnd = false;
-
-          std::vector<Value*> inst(0);
-          CallInst::Create(RtnExitFn, inst.begin(), inst.end(), "", BI);
-          unknown = false;
-        }
-
-        if (unknown) {
-          // do nothing
-          End = BI;
-          validEnd = true;
-        }
-      }
-      if (validBegin) {
-        End = BB->end();
-        runOnBasicBlockSlice(M, Begin, End, first_dtor_bb);
       }
     }
 
