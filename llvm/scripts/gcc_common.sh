7efbb56475a2f0a2955b62a6fd102928b5e96b6c
ramosian.glider <ramosian.glider@93a3164d-3a44-0410-8ae5-4baa038b0729>
2011-01-13 13:44:41 +0000

    Skip the instrumentation when compiling a .S file directly
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@3019 93a3164d-3a44-0410-8ae5-4baa038b0729

--- llvm/scripts/gcc_common.sh
+++ llvm/scripts/gcc_common.sh
@@ -10,12 +10,14 @@ ARGS=
 LD_MODE=
 # Optimization level: -O0, -O1 etc.
 OX=-O0
+OPT_OX=
 # -fPIC flag.
 FPIC=
 # llc analog of -fPIC is -relocation-model=pic
 LLC_PIC=
-OPT_OX=
 DEBUG=
+# If true, we're compiling an assembly file.
+FROM_ASM=
 OPT_PASSES=-adce
 OPT_PASSES="-reg2mem -mem2reg -adce"
 OPT_PASSES=-verify
@@ -32,6 +34,10 @@ do
   elif [ `expr match "$1" ".*\.c"` -gt 0 ]
   then
     SRC="$1"
+  elif [ `expr match "$1" ".*\.S"` -gt 0 ]
+  then
+    SRC="$1"
+    FROM_ASM=1
   elif [ `expr match "$1" "-o"` -gt 0 ]
   then
     if [ "$1" == "-o" ]
@@ -106,27 +112,31 @@ LOG=instrumentation.log
 
 set_platform_dependent_vars
 
-if [ "$COMPILE_FAST" == "1" ]
+if [ "$FROM_ASM" != "1" ]
 then
-# Translate C code to LLVM bitcode.
-$COMPILER -emit-llvm $MARCH $SRC $OX $FPIC $DEBUG -c $DA_FLAGS $ARGS -o "$SRC_BIT" ||   exit 1
-# Instrument the bitcode.
-$OPT $OPT_PASSES -load "$PASS_SO" $INST_MODE -arch=$XARCH "$SRC_BIT" -o "$SRC_INSTR" 2>$LOG || exit 1
-else
-# Translate C code to LLVM bitcode.
-$COMPILER -emit-llvm $MARCH $SRC $OX $FPIC $DEBUG -S $DA_FLAGS $ARGS -o "$SRC_BIT" || exit 1
-# Instrument the bitcode.
-$OPT $OPT_PASSES  "$SRC_BIT" $FPIC -S  > "$SRC_TMP" 2>$LOG || exit 1
-$OPT -load "$PASS_SO" $INST_MODE -arch=$XARCH $FPIC "$SRC_TMP" -S  > "$SRC_INSTR" 2>$LOG || exit 1
+  if [ "$COMPILE_FAST" == "1" ]
+  then
+  # Translate C code to LLVM bitcode.
+  $COMPILER -emit-llvm $MARCH $SRC $OX $FPIC $DEBUG -c $DA_FLAGS $ARGS -o "$SRC_BIT" ||   exit 1
+  # Instrument the bitcode.
+  $OPT $OPT_PASSES -load "$PASS_SO" $INST_MODE -arch=$XARCH "$SRC_BIT" -o "$SRC_INSTR" 2>$LOG || exit 1
+  else
+  # Translate C code to LLVM bitcode.
+  $COMPILER -emit-llvm $MARCH $SRC $OX $FPIC $DEBUG -S $DA_FLAGS $ARGS -o "$SRC_BIT" || exit 1
+  # Instrument the bitcode.
+  $OPT $OPT_PASSES  "$SRC_BIT" $FPIC -S  > "$SRC_TMP" 2>$LOG || exit 1
+  $OPT -load "$PASS_SO" $INST_MODE -arch=$XARCH $FPIC "$SRC_TMP" -S  > "$SRC_INSTR" 2>$LOG || exit 1
+  fi
+
+  # Translate LLVM bitcode to native assembly code.
+  $LLC -march=$XARCH $LLC_PIC $OX $SRC_INSTR  -o $SRC_ASM
+  if [ "$?" != "0" ]
+  then
+    $FALLBACK_COMPILER $MARCH $SRC $OX $FPIC $DEBUG -c $DA_FLAGS $ARGS -o $SRC_OBJ
+    exit 0
+  fi
 fi
 
-# Translate LLVM bitcode to native assembly code.
-$LLC -march=$XARCH $LLC_PIC $OX $SRC_INSTR  -o $SRC_ASM
-if [ "$?" != "0" ]
-then
-  $FALLBACK_COMPILER $MARCH $SRC $OX $FPIC $DEBUG -c $DA_FLAGS $ARGS -o $SRC_OBJ
-  exit 0
-else
-  # Compile the object file.
-  $COMPILER $MARCH -c $SRC_ASM $OX $FPIC $DEBUG -o $SRC_OBJ || $FALLBACK_COMPILER $MARCH $SRC $OX $FPIC $DEBUG -c $DA_FLAGS $ARGS -o $SRC_OBJ ||   exit 1
-fi
+# Compile the object file.
+$COMPILER $MARCH -c $SRC_ASM $OX $FPIC $DEBUG -o $SRC_OBJ || $FALLBACK_COMPILER $MARCH $SRC $OX $FPIC $DEBUG -c $DA_FLAGS $ARGS -o $SRC_OBJ ||   exit 1
+
