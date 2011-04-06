05986ad1159d3deedf13e48e3bcedc78387714bf
ramosian.glider <ramosian.glider@93a3164d-3a44-0410-8ae5-4baa038b0729>
2011-04-06 08:34:02 +0000

    Add the copyright information.
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@3287 93a3164d-3a44-0410-8ae5-4baa038b0729

--- llvm/tsan_rtl/tsan_rtl.cc
+++ llvm/tsan_rtl/tsan_rtl.cc
@@ -1,3 +1,31 @@
+/* Copyright (c) 2010-2011, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// Author: glider@google.com (Alexander Potapenko)
+
 #include "tsan_rtl.h"
 
 #include "ts_trace_info.h"
@@ -6,8 +34,6 @@
 #include <elf.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-
-
 
 #ifdef TSAN_RTL_X64
 #include <asm/prctl.h>
@@ -33,7 +59,6 @@
 int static_tls_size;
 // Reentrancy counter
 __thread int IN_RTL = 0;
-
 
 extern bool global_ignore;
 bool FORKED_CHILD = false;  // if true, cannot access other threads' TLS
