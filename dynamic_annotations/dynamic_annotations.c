ad849b846dd13e972a6ca783c69f1892fd1dfac3
eugenis@google.com <eugenis@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2011-04-06 13:28:56 +0000

    Handle case when __COUNTER__ is not available.
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@3294 93a3164d-3a44-0410-8ae5-4baa038b0729

--- dynamic_annotations/dynamic_annotations.c
+++ dynamic_annotations/dynamic_annotations.c
@@ -51,7 +51,11 @@
 
 /* Identical code folding(-Wl,--icf=all) countermeasures.
    This makes all Annotate* functions different, which prevents the linker from folding them. */
+#ifdef __COUNTER__
 #define DYNAMIC_ANNOTATIONS_IMPL volatile short lineno = (__LINE__ << 8) + __COUNTER__; (void)lineno;
+#else
+#define DYNAMIC_ANNOTATIONS_IMPL volatile short lineno = (__LINE__ << 8); (void)lineno;
+#endif
 
 /* WARNING: always add new annotations to the end of the list.
    Otherwise, lineno (see above) numbers for different Annotate* functions may conflict. */
