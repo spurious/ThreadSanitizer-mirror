f635fad48e0abd386d75a5cdb2f1e11a4fc96469
dvyukov@google.com <dvyukov@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2011-05-13 15:00:34 +0000

    Spin lock support
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@3442 93a3164d-3a44-0410-8ae5-4baa038b0729

--- tsan/ts_util.h
+++ tsan/ts_util.h
@@ -332,6 +332,7 @@ class ThreadSanitizerReport;
 // Time since some moment before the program start.
 extern size_t TimeInMilliSeconds();
 extern void YIELD();
+extern void PROCESSOR_YIELD();
 
 extern "C" long my_strtol(const char *str, char **end, int base);
 extern void Printf(const char *format, ...);
