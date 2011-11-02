83daccd75e30eef93aefa077d3ccf27d560997ac
mpimenov@google.com <mpimenov@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2011-11-02 15:24:06 +0000

    slight change to go_rtl
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@3686 93a3164d-3a44-0410-8ae5-4baa038b0729

--- go/go_rtl.cc
+++ go/go_rtl.cc
@@ -12,6 +12,9 @@
 #include "ts_util.h"
 
 
+bool in_initialize = false;
+
+
 void PcToStrings(uintptr_t pc, bool demangle,
                  string *img_name, string *rtn_name,
                  string *file_name, int *line_no) {
@@ -20,6 +23,49 @@ void PcToStrings(uintptr_t pc, bool demangle,
 string PcToRtnName(uintptr_t pc, bool demangle) {
 
 }
+
+extern "C" {
+
+bool initialize() {
+  if (in_initialize) return false;
+  in_initialize = true;
+
+  G_flags = new FLAGS;
+  vector<string> args;
+
+  char *env = getenv("TSAN_ARGS");
+  if (env) {
+    string env_args(const_cast<char*>(env));
+    size_t start = env_args.find_first_not_of(" ");
+    size_t stop = env_args.find_first_of(" ", start);
+    while (start != string::npos || stop != string::npos) {
+      args.push_back(env_args.substr(start, stop - start));
+      start = env_args.find_first_not_of(" ", stop);
+      stop = env_args.find_first_of(" ", start);
+    }
+  }
+
+  ThreadSanitizerParseFlags(&args);
+  ThreadSanitizerInit();
+
+  return true;
+}
+
+}
+
+
+extern "C" {
+
+void finalize() {
+  ThreadSanitizerFini();
+  if (G_flags->error_exitcode && GetNumberOfFoundErrors() > 0) {
+    // This is the last atexit hook, so it's ok to terminate the program.
+    _exit(G_flags->error_exitcode);
+  }
+}
+
+}
+
 
 extern "C" {
 
