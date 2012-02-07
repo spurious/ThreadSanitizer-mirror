65ba08e493908942d24ba77465eeca3832e82bc4
konstantin.s.serebryany@gmail.com <konstantin.s.serebryany@gmail.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2012-02-07 04:12:08 +0000

    [v2] add tsan_linux.h (forgot last time)
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@3785 93a3164d-3a44-0410-8ae5-4baa038b0729

---
+++ v2/tsan_linux.h
@@ -0,0 +1,51 @@
+//===-- tsan_linux.h --------------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is a part of ThreadSanitizer (TSan), a race detector.
+//
+// Linux-specific code.
+//===----------------------------------------------------------------------===//
+
+#ifndef TSAN_LINUX_H
+#define TSAN_LINUX_H
+#ifdef __linux__
+
+#include "tsan_rtl.h"
+
+#if __LP64__
+namespace __tsan {
+
+static const uptr kLinuxAppMemBeg = 0x7f0000000000ULL;
+static const uptr kLinuxAppMemEnd = 0x7fffffffffffULL;
+static inline bool IsAppMem(uptr mem) {
+  return mem >= kLinuxAppMemBeg && mem <= kLinuxAppMemEnd;
+}
+
+#ifndef TSAN_SHADOW_STATE_LENGTH
+#define TSAN_SHADOW_STATE_LENGTH 8
+#endif
+
+#define MemToShadow(mem) \
+  (((mem) & (~0x7c0000000003ULL)) * TSAN_SHADOW_STATE_LENGTH)
+static const uptr kLinuxShadowBeg = MemToShadow(kLinuxAppMemBeg);
+static const uptr kLinuxShadowEnd =
+  MemToShadow(kLinuxAppMemEnd) | (kPageSize - 1);
+
+static inline bool IsShadowMem(uptr mem) {
+  return mem >= kLinuxShadowBeg && mem <= kLinuxShadowEnd;
+}
+
+}  // namespace __tsan
+
+#else  // __LP64__
+# error "Only 64-bit is supported"
+#endif
+
+#endif  // __linux__
+#endif  // TSAN_LINUX_H
