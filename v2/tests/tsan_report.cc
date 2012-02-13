38298a35f51c512c8317989ca3c4086c4e7ee8bf
dvyukov@google.com <dvyukov@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2012-02-13 19:13:34 +0000

    [v2] add a test that verifies report stacks
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@3883 93a3164d-3a44-0410-8ae5-4baa038b0729

--- v2/tests/tsan_report.cc
+++ v2/tests/tsan_report.cc
@@ -10,6 +10,7 @@
 // This file is a part of ThreadSanitizer (TSan), a race detector.
 //
 //===----------------------------------------------------------------------===//
+#include "tsan_interface.h"
 #include "tsan_test_util.h"
 #include "tsan_defs.h"
 #include "tsan_report.h"
@@ -49,4 +50,50 @@ TEST(ThreadSanitizer, ReportBasic) {
   CHECK_EQ(rep->loc, NULL);
   CHECK_EQ(rep->nthread, 0);
   CHECK_EQ(rep->nmutex, 0);
+}
+
+static uintptr_t NOINLINE get_pc() {
+  return (uintptr_t)__builtin_return_address(0);
+}
+
+static void foo() {}; static int foo_line = __LINE__;
+static void bar() {}; static int bar_line = __LINE__;
+
+TEST(ThreadSanitizer, ReportStack) {
+  ScopedThread t1;
+  MemLoc l;
+  const char *func = __FUNCTION__;
+  const char *file = __FILE__;
+  uintptr_t pc = get_pc();
+  __tsan_write1(l.loc()); int line = __LINE__;
+  t1.Call(&foo);
+  t1.Call(&bar);
+  const ReportDesc *rep = t1.Write1(l, true);
+  CHECK_EQ(rep->typ, __tsan::ReportTypeRace);
+  CHECK_EQ(rep->nmop, 2);
+  CHECK_NE(rep->mop[0].tid, 0);
+  CHECK_EQ(rep->mop[0].addr, (uintptr_t)l.loc());
+  CHECK_EQ(rep->mop[0].size, 1);
+  CHECK_EQ(rep->mop[0].write, true);
+  CHECK_EQ(rep->mop[0].stack.cnt, 3);
+  CHECK_EQ(rep->mop[0].stack.entry[1].pc, (uintptr_t)(void*)&bar);
+  CHECK(strstr(rep->mop[0].stack.entry[1].func, "bar"));
+  CHECK(strstr(rep->mop[0].stack.entry[1].file, file));
+  CHECK_EQ(rep->mop[0].stack.entry[1].line, bar_line);
+  CHECK_EQ(rep->mop[0].stack.entry[2].pc, (uintptr_t)(void*)&foo);
+  CHECK(strstr(rep->mop[0].stack.entry[2].func, "foo"));
+  CHECK(strstr(rep->mop[0].stack.entry[2].file, file));
+  CHECK_EQ(rep->mop[0].stack.entry[2].line, foo_line);
+  CHECK_EQ(rep->mop[1].tid, 0);
+  CHECK_EQ(rep->mop[1].addr, (uintptr_t)l.loc());
+  CHECK_EQ(rep->mop[1].size, 1);
+  CHECK_EQ(rep->mop[1].write, true);
+  CHECK_EQ(rep->mop[1].stack.cnt, 1);
+  CHECK_GT(rep->mop[1].stack.entry[0].pc, pc - 64);
+  CHECK_LT(rep->mop[1].stack.entry[0].pc, pc + 64);
+  CHECK(strstr(rep->mop[1].stack.entry[0].func, func));
+  CHECK(strstr(rep->mop[1].stack.entry[0].file, file));
+  CHECK_EQ(rep->mop[1].stack.entry[0].line, line);
+  (void)foo_line;
+  (void)bar_line;
 }
