f90e9c7df9a010a88aae5d93a3bcbc0218f41582
dvyukov@google.com <dvyukov@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2012-02-13 16:17:22 +0000

    [v2] add comment for "offsetting" trick
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@3874 93a3164d-3a44-0410-8ae5-4baa038b0729

--- v2/tsan/tsan_rtl.cc
+++ v2/tsan/tsan_rtl.cc
@@ -24,7 +24,7 @@
 
 namespace __tsan {
 
-static __thread __tsan::ThreadState cur_thread;
+__thread __tsan::ThreadState cur_thread;
 
 union Shadow {
   struct {
@@ -349,6 +349,20 @@ void MemoryAccess(ThreadState *thr, uptr pc, uptr addr,
   // 'candidates' with 'same' or 'replace', but I think
   // it's just not worth it (performance- and complexity-wise).
   const u64 synch_epoch = thr->fast_synch_epoch;
+
+  // The idea behind the offset is as follows.
+  // Consider that we have 8 bool's contained within a single 8-byte block
+  // (mapped to a single shadow "cell"). Now consider that we write to the bools
+  // from a single thread (which we consider the common case).
+  // W/o offsetting each access will have to scan 4 shadow values at average
+  // to find the corresponding shadow value for the bool.
+  // With offsetting we start scanning shadow with the offset so that
+  // each access hits necessary shadow straight off (at least in an expected
+  // optimistic case).
+  // This logic works seamlessly for any layout of user data. For example,
+  // if user data is {int, short, char, char}, then accesses to the int are
+  // offsetted to 0, short - 4, 1st char - 6, 2nd char - 7. Hopefully, accesses
+  // from a single thread won't need to scan all 8 shadow values.
   int off = 0;
   if (size == 1)
     off = addr & 7;
@@ -356,7 +370,7 @@ void MemoryAccess(ThreadState *thr, uptr pc, uptr addr,
     off = addr & 6;
   else if (size == 4)
     off = addr & 4;
-  shadow_mem += off;
+
   for (int i = 0; i < kShadowCnt; i++) {
     u64 *sp = &shadow_mem[(i + off) % kShadowCnt];
     if (MemoryAccess1(thr, fast_state, synch_epoch, s0, sp, is_write,
