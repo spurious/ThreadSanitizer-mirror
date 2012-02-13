767925928312475d5f34f071db3a2f511f22f901
dvyukov@google.com <dvyukov@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2012-02-13 19:54:16 +0000

    [v2] move mop tests to a separate file
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@3885 93a3164d-3a44-0410-8ae5-4baa038b0729

--- v2/tests/tsan_mop.cc
+++ v2/tests/tsan_mop.cc
@@ -1,4 +1,4 @@
-//===-- tsan_test.cc --------------------------------------------*- C++ -*-===//
+//===-- tsan_mop.cc ---------------------------------------------*- C++ -*-===//
 //
 //                     The LLVM Compiler Infrastructure
 //
@@ -14,46 +14,43 @@
 #include "tsan_test_util.h"
 #include "gtest/gtest.h"
 
-static void foo() {}
-static void bar() {}
-
-TEST(ThreadSanitizer, FuncCall) {
-  ScopedThread t1, t2;
-  MemLoc l;
-  t1.Write1(l);
-  t2.Call(foo);
-  t2.Call(bar);
-  t2.Write1(l, true);
-  t2.Return();
-  t2.Return();
-}
-
-TEST(ThreadSanitizer, SimpleMutex) {
+TEST(ThreadSanitizer, SimpleWrite) {
   ScopedThread t;
-  Mutex m;
-  t.Create(m);
-  t.Lock(m);
-  t.Unlock(m);
-  t.Destroy(m);
+  MemLoc l;
+  t.Write1(l);
 }
 
-TEST(ThreadSanitizer, Mutex) {
+TEST(ThreadSanitizer, SimpleWriteWrite) {
+  ScopedThread t1, t2;
+  MemLoc l1, l2;
+  t1.Write1(l1);
+  t2.Write1(l2);
+}
+
+TEST(ThreadSanitizer, WriteWriteRace) {
   ScopedThread t1, t2;
   MemLoc l;
-  Mutex m;
-  t1.Create(m);
-  t1.Lock(m);
   t1.Write1(l);
-  t1.Unlock(m);
-  t2.Lock(m);
-  t2.Write1(l);
-  t2.Unlock(m);
-  t2.Destroy(m);
+  t2.Write1(l, true);
 }
 
-int main(int argc, char **argv) {
-  __tsan_init();
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-  return 0;
+TEST(ThreadSanitizer, ReadWriteRace) {
+  ScopedThread t1, t2;
+  MemLoc l;
+  t1.Read1(l);
+  t2.Write1(l, true);
+}
+
+TEST(ThreadSanitizer, WriteReadRace) {
+  ScopedThread t1, t2;
+  MemLoc l;
+  t1.Write1(l);
+  t2.Read1(l, true);
+}
+
+TEST(ThreadSanitizer, ReadReadNoRace) {
+  ScopedThread t1, t2;
+  MemLoc l;
+  t1.Read1(l);
+  t2.Read1(l);
 }
