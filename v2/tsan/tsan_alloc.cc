555c64bb879a4dfc7b1caf6cfd780536c864ead6
dvyukov@google.com <dvyukov@google.com@93a3164d-3a44-0410-8ae5-4baa038b0729>
2012-03-01 13:34:38 +0000

    [v2] add stub file for memory allocator
    git-svn-id: http://data-race-test.googlecode.com/svn/trunk@3973 93a3164d-3a44-0410-8ae5-4baa038b0729

---
+++ v2/tsan/tsan_alloc.cc
@@ -0,0 +1,17 @@
+//===-- tsan_alloc.cc -------------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is a part of ThreadSanitizer (TSan), a race detector.
+//
+//===----------------------------------------------------------------------===//
+#include "tsan_alloc.h"
+
+namespace __tsan {
+
+}  // namespace __tsan
